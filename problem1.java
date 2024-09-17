51c0f7d9f8fa6d67cea5f1dd8dc3cd6f39dbd869
Jamiee-N <jpnguyen124@student.hbuhsd.edu>
2024-09-16 23:44:26 -0700

    Update problem1.java

--- problem1.java
+++ problem1.java
@@ -1,1 +1,8 @@
-// Create your class here; don't forget the main method
+public class problem1
+{
+  public static void main(String[] args)
+  {
+    var food = ice cream
+    System.out.print("I like to eat " + food + " as well!");
+  }
+}
